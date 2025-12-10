52be88d5373c1d17ed4fb86c8b824532fa53c8d1
Saif Ullah <s.ullah16@studenti.unisa.it>
2025-12-10 22:49:59 +0200

    Jacoco Test Case Run

--- src/test/java/crud/springboot/SpringbootThymeleafCrudWebAppApplicationTests.java
+++ src/test/java/crud/springboot/SpringbootThymeleafCrudWebAppApplicationTests.java
@@ -2,12 +2,14 @@ package crud.springboot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test") // Use application-test.properties
 class SpringbootThymeleafCrudWebAppApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+        // Test will pass if Spring context loads successfully
+    }
 }
